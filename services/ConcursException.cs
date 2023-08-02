83a4a631bee88f36a602253a9367bb399a88eac6
Marius Andreiasi <126679825+Marius44KT@users.noreply.github.com>
2023-08-03 00:20:43 +0300

    Add files via upload

---
+++ services/ConcursException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Permissions;
+
+namespace services;
+
+public class ConcursException:Exception
+{
+    public ConcursException() { }
+
+    public ConcursException(string message):base(message) { }
+
+    public ConcursException(String message,Exception innerException):base(message,innerException){}
+}
---
+++ services/IServices.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace services;
+
+public interface IServices
+{
+    bool login(string date, string password, IObserver client);
+
+    List<ParticipantDTO> getParticipanti();
+    
+    List<ParticipantDTO> getSearchedParticipanti(string stil,string distanta);
+
+    List<int> getNumarParticipantiDupaProba();
+
+    void inscriereParticipant(string nume, int varsta, string stil, string distanta);
+    
+    void logout(string email);
+}
